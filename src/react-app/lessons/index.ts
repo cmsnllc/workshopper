9b8bd347311c48cfe8d8e72725ce515f508bbaf5
jigsaw <m@jgs.me>
2025-11-13 14:58:06 +0900

    save

--- src/react-app/lessons/index.ts
+++ src/react-app/lessons/index.ts
@@ -1,25 +1,70 @@
 import type { Lesson } from "./types";
-import LessonContent0101, {
-  frontmatter as meta0101,
-} from "./01-first-circle.mdx";
-import LessonContent0102, {
-  frontmatter as meta0102,
-} from "./01-02-basics.mdx";
-import LessonContent0103, {
-  frontmatter as meta0103,
-} from "./01-03-challenge-yellow.mdx";
+import LessonContent0101, { frontmatter as meta0101 } from "./01-first-circle.mdx";
+import LessonContent0102, { frontmatter as meta0102 } from "./01-02-basics.mdx";
+import LessonContent0103, { frontmatter as meta0103 } from "./01-03-challenge-yellow.mdx";
+import LessonContent0201, { frontmatter as meta0201 } from "./02-01-multiple-shapes.mdx";
+import LessonContent0202, { frontmatter as meta0202 } from "./02-02-challenge-traffic.mdx";
+import LessonContent0301, { frontmatter as meta0301 } from "./03-01-variables.mdx";
+import LessonContent0302, { frontmatter as meta0302 } from "./03-02-challenge-move.mdx";
+import LessonContent0401, { frontmatter as meta0401 } from "./04-01-arithmetic.mdx";
+import LessonContent0402, { frontmatter as meta0402 } from "./04-02-challenge-allowance.mdx";
+import LessonContent0501, { frontmatter as meta0501 } from "./05-01-comparison.mdx";
+import LessonContent0502, { frontmatter as meta0502 } from "./05-02-challenge-even.mdx";
+import LessonContent0601, { frontmatter as meta0601 } from "./06-01-if-statement.mdx";
+import LessonContent0602, { frontmatter as meta0602 } from "./06-02-challenge-conditional.mdx";
+import LessonContent0701, { frontmatter as meta0701 } from "./07-01-logical-operators.mdx";
+import LessonContent0702, { frontmatter as meta0702 } from "./07-02-challenge-complex.mdx";
+import LessonContent0801, { frontmatter as meta0801 } from "./08-01-loops-intro.mdx";
+import LessonContent0802, { frontmatter as meta0802 } from "./08-02-loops-practice.mdx";
+import LessonContent0803, { frontmatter as meta0803 } from "./08-03-challenge-loops.mdx";
+import LessonContent0901, { frontmatter as meta0901 } from "./09-01-nested-loops.mdx";
+import LessonContent0902, { frontmatter as meta0902 } from "./09-02-challenge-dots.mdx";
+import LessonContent1001, { frontmatter as meta1001 } from "./10-01-animation-intro.mdx";
+import LessonContent1002, { frontmatter as meta1002 } from "./10-02-animation-practice.mdx";
+import LessonContent1003, { frontmatter as meta1003 } from "./10-03-challenge-bounce.mdx";
+import LessonContent1101, { frontmatter as meta1101 } from "./11-01-random.mdx";
+import LessonContent1102, { frontmatter as meta1102 } from "./11-02-challenge-random.mdx";
+import LessonContent1201, { frontmatter as meta1201 } from "./12-01-mouse-events.mdx";
+import LessonContent1202, { frontmatter as meta1202 } from "./12-02-challenge-paint.mdx";
+import LessonContent1301, { frontmatter as meta1301 } from "./13-01-functions.mdx";
+import LessonContent1302, { frontmatter as meta1302 } from "./13-02-challenge-function.mdx";
+import LessonContent1401, { frontmatter as meta1401 } from "./14-01-recursion-intro.mdx";
+import LessonContent1402, { frontmatter as meta1402 } from "./14-02-recursion-practice.mdx";
+import LessonContent1403, { frontmatter as meta1403 } from "./14-03-challenge-recursion.mdx";
+import LessonContent1501, { frontmatter as meta1501 } from "./15-01-free-project.mdx";
 
 export const lessons: Lesson[] = [
-  {
-    meta: meta0101,
-    Content: LessonContent0101,
-  },
-  {
-    meta: meta0102,
-    Content: LessonContent0102,
-  },
-  {
-    meta: meta0103,
-    Content: LessonContent0103,
-  },
+  { meta: meta0101, Content: LessonContent0101 },
+  { meta: meta0102, Content: LessonContent0102 },
+  { meta: meta0103, Content: LessonContent0103 },
+  { meta: meta0201, Content: LessonContent0201 },
+  { meta: meta0202, Content: LessonContent0202 },
+  { meta: meta0301, Content: LessonContent0301 },
+  { meta: meta0302, Content: LessonContent0302 },
+  { meta: meta0401, Content: LessonContent0401 },
+  { meta: meta0402, Content: LessonContent0402 },
+  { meta: meta0501, Content: LessonContent0501 },
+  { meta: meta0502, Content: LessonContent0502 },
+  { meta: meta0601, Content: LessonContent0601 },
+  { meta: meta0602, Content: LessonContent0602 },
+  { meta: meta0701, Content: LessonContent0701 },
+  { meta: meta0702, Content: LessonContent0702 },
+  { meta: meta0801, Content: LessonContent0801 },
+  { meta: meta0802, Content: LessonContent0802 },
+  { meta: meta0803, Content: LessonContent0803 },
+  { meta: meta0901, Content: LessonContent0901 },
+  { meta: meta0902, Content: LessonContent0902 },
+  { meta: meta1001, Content: LessonContent1001 },
+  { meta: meta1002, Content: LessonContent1002 },
+  { meta: meta1003, Content: LessonContent1003 },
+  { meta: meta1101, Content: LessonContent1101 },
+  { meta: meta1102, Content: LessonContent1102 },
+  { meta: meta1201, Content: LessonContent1201 },
+  { meta: meta1202, Content: LessonContent1202 },
+  { meta: meta1301, Content: LessonContent1301 },
+  { meta: meta1302, Content: LessonContent1302 },
+  { meta: meta1401, Content: LessonContent1401 },
+  { meta: meta1402, Content: LessonContent1402 },
+  { meta: meta1403, Content: LessonContent1403 },
+  { meta: meta1501, Content: LessonContent1501 },
 ];
